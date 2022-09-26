2bb614022f10af58370d1a7325ce445d4a6ee95c
Roy Sun <sunj@avonoldfarms.com>
2022-09-26 13:16:32 -0400

    inittial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import meme from "./xb.jpeg";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.redBox}>
+      <Text style={styles.text}>CS is so fun!</Text>
+      </View>
+      <Text style={styles.text2}> Mr. Sayles is the best teacher</Text>
+      <Text style={styles.text3}> Idk what am I writing</Text>
+      <TouchableOpacity onPress={() => alert("你惊扰了witch！")}>
+        <Image source={meme} style={styles.image} />
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  redBox: {
+    backgroundColor: '#ff0049',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "#00ff00",
+    fontsize: 29,
+  },
+  text2: {
+    color: "#ff0000",
+    fontsize: 9,
+  },
+  image: {
+    width: 195,
+    height: 258,
+  }
+});
