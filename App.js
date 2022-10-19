248a087523dde58662b0ba6dc9f47f119561c727
Roy Sun <sunj@avonoldfarms.com>
2022-10-19 08:21:13 -0400

    Adventure Game

--- App.js
+++ App.js
@@ -1,34 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import meme from "./xb.jpeg";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.redBox}>
-      <Text style={styles.text}>CS is so fun!</Text>
+        <Text style={styles.text}>CS is so fun!</Text>
       </View>
       <Text style={styles.text2}> Mr. Sayles is the best teacher</Text>
-      <Text style={styles.text3}> Idk what am I writing</Text>
-      <TouchableOpacity onPress={() => alert("你惊扰了witch！")}>
+      <Text style={styles.text2}> Idk what am I writing</Text>
+      <TouchableOpacity onPress={() => alert("你惊扰了witch")}>
         <Image source={meme} style={styles.image} />
       </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   redBox: {
-    backgroundColor: '#ff0049',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ff0049",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     color: "#00ff00",
@@ -41,5 +48,5 @@ const styles = StyleSheet.create({
   image: {
     width: 195,
     height: 258,
-  }
+  },
 });
